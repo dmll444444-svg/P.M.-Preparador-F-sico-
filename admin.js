c75e237f3c662f6e9f30b81457d698114c3182d7
dmll444444-svg <dmll444444@gmail.com>
2026-06-07 21:03:22 +0200

    Fix cliente dashboard PM

--- admin.js
+++ admin.js
@@ -4117,12 +4117,6 @@ const sections = {
           </div>
         </div>
 
-        <div class="valuation-table-head">
-          <span>TEST 1</span>
-          <span>Datos registrados</span>
-          <span>Registro</span>
-          <span>Observaciones cualitativas</span>
-        </div>
 
         <div class="valuation-tests-area" id="valuationTestsArea">
           ${valuationTestRow(1)}
